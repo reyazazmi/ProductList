47886bec7eabd05923807fa498856b0fdc1451ba
Md Reyaz <mdreyaz@publicisgroupe.net>
2021-09-11 16:52:54 +0530

    db changes schema and rows added

--- crud_db.sql
+++ crud_db.sql
@@ -17,39 +17,75 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `crud_db`
---
+-- Database: `my_db`
+CREATE SCHEMA `my_db` ;
+----------------------------------------
+CREATE TABLE IF NOT EXISTS `my_db`.`category` (
+  `category_id` INT NOT NULL,
+  `category_name` VARCHAR(45) NULL,
+  PRIMARY KEY (`category_id`));
+----------------------------------------
+CREATE TABLE IF NOT EXISTS `my_db`.`product` (
+  `product_id` INT NOT NULL AUTO_INCREMENT,
+  `product_name` VARCHAR(45) NULL,
+  `product_price` INT NULL,
+  `category_id` INT NULL,
+  PRIMARY KEY (`product_id`),
+  INDEX `category_id_idx` (`category_id` ASC) VISIBLE,
+  CONSTRAINT `category_id`
+    FOREIGN KEY (`category_id`)
+    REFERENCES `my_db`.`category` (`category_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+	
+----------------------
+ALTER TABLE `my_db`.`product` 
+DROP FOREIGN KEY `category_id`;
+ALTER TABLE `my_db`.`product` 
+ADD CONSTRAINT `category_id`
+  FOREIGN KEY (`category_id`)
+  REFERENCES `my_db`.`category` (`category_id`)
+  ON DELETE RESTRICT
+  ON UPDATE CASCADE;
 
--- --------------------------------------------------------
+-- ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root123';
+-- flush privileges;
 
---
--- Struktur dari tabel `product`
---
 
--- CREATE TABLE IF NOT EXISTS `product` (
---   `product_id` int(11) NOT NULL AUTO_INCREMENT,
---   `product_name` varchar(200) DEFAULT NULL,
---   `product_price` int(11) DEFAULT NULL,
---   PRIMARY KEY (`product_id`)
--- ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
--- CREATE TABLE IF NOT EXISTS `product` (
---   `product_id` int(11) NOT NULL AUTO_INCREMENT,
---   `product_name` varchar(200) DEFAULT NULL,
---   `product_price` int(11) DEFAULT NULL,
---   PRIMARY KEY (`product_id`)
--- ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
-
---
--- Dumping data untuk tabel `product`
---
-
--- INSERT INTO `product` (`product_name`, `cat_id`) VALUES
--- ('Product 1', 2000),
--- ('Product 2', 2000),
--- ('Product 3', 3000),
--- ('Product 4', 2000),
--- ('Product 5', 1500);
+INSERT INTO `my_db`.`product`(`product_name`,`product_price`,`category_id`)VALUES('p1',100000,11);
+INSERT INTO `my_db`.`product`(`product_name`,`product_price`,`category_id`)VALUES('p2',150,22);
+INSERT INTO `my_db`.`product`(`product_name`,`product_price`,`category_id`)VALUES('p3',2000,33);
+INSERT INTO `my_db`.`product`(`product_name`,`product_price`,`category_id`)VALUES('p4',100,44);
+INSERT INTO `my_db`.`product`(`product_name`,`product_price`,`category_id`)VALUES('p5',100000,11);
+INSERT INTO `my_db`.`product`(`product_name`,`product_price`,`category_id`)VALUES('p6',150,22);
+INSERT INTO `my_db`.`product`(`product_name`,`product_price`,`category_id`)VALUES('p7',2000,33);
+INSERT INTO `my_db`.`product`(`product_name`,`product_price`,`category_id`)VALUES('p8',100,44);
+INSERT INTO `my_db`.`product`(`product_name`,`product_price`,`category_id`)VALUES('p9',100000,11);
+INSERT INTO `my_db`.`product`(`product_name`,`product_price`,`category_id`)VALUES('p10',150,22);
+------------------
+INSERT INTO `my_db`.`product`(`product_name`,`product_price`,`category_id`)VALUES('p11',100000,11);
+INSERT INTO `my_db`.`product`(`product_name`,`product_price`,`category_id`)VALUES('p12',150,22);
+INSERT INTO `my_db`.`product`(`product_name`,`product_price`,`category_id`)VALUES('p13',2000,33);
+INSERT INTO `my_db`.`product`(`product_name`,`product_price`,`category_id`)VALUES('p14',100,44);
+INSERT INTO `my_db`.`product`(`product_name`,`product_price`,`category_id`)VALUES('p15',100000,11);
+INSERT INTO `my_db`.`product`(`product_name`,`product_price`,`category_id`)VALUES('p16',150,22);
+INSERT INTO `my_db`.`product`(`product_name`,`product_price`,`category_id`)VALUES('p17',2000,33);
+INSERT INTO `my_db`.`product`(`product_name`,`product_price`,`category_id`)VALUES('p18',100,44);
+INSERT INTO `my_db`.`product`(`product_name`,`product_price`,`category_id`)VALUES('p19',100000,11);
+INSERT INTO `my_db`.`product`(`product_name`,`product_price`,`category_id`)VALUES('p20',150,22);
+------
+INSERT INTO `my_db`.`product`(`product_name`,`product_price`,`category_id`)VALUES('p21',100000,11);
+INSERT INTO `my_db`.`product`(`product_name`,`product_price`,`category_id`)VALUES('p22',150,22);
+INSERT INTO `my_db`.`product`(`product_name`,`product_price`,`category_id`)VALUES('p23',2000,33);
+INSERT INTO `my_db`.`product`(`product_name`,`product_price`,`category_id`)VALUES('p24',100,44);
+INSERT INTO `my_db`.`product`(`product_name`,`product_price`,`category_id`)VALUES('p25',100000,11);
+INSERT INTO `my_db`.`product`(`product_name`,`product_price`,`category_id`)VALUES('p26',150,22);
+INSERT INTO `my_db`.`product`(`product_name`,`product_price`,`category_id`)VALUES('p27',2000,33);
+INSERT INTO `my_db`.`product`(`product_name`,`product_price`,`category_id`)VALUES('p28',100,44);
+INSERT INTO `my_db`.`product`(`product_name`,`product_price`,`category_id`)VALUES('p29',100000,11);
+INSERT INTO `my_db`.`product`(`product_name`,`product_price`,`category_id`)VALUES('p30',150,22);
+commit;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
